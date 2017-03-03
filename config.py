b014c3acd7da46cfbaf7d02c423d34f57ac57aa0
Ellis Brown <elbrown47@gmail.com>
2017-03-03 01:54:17 -0600

    update config to take automatic VOCroot path from download scripts

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # config.py
+import os.path
 
+# gets home dir cross platform
+home = os.path.expanduser("~")
+ddir = os.path.join(home,"data/VOCdevkit/")
 
-VOCroot = "/Users/ellisbrown/data/VOCdevkit/" # path to VOCdevkit root dir
+# note: if you used our download scripts, this should be right
+VOCroot = ddir # path to VOCdevkit root dir
 
