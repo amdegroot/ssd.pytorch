cd6156bb53e42629011f370b4799fec031ad38b4
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-03-03 15:57:26 -0600

    demo notebook tweak model def

--- detection_output.py
+++ detection_output.py
@@ -6,90 +6,66 @@ from torch.autograd import Variable
 from utils import *
 
 class Detect(Function):
-    def __init__(self, num_classes_, share_location_, background_label_id_, code_type_, var_encoded_in_target_, keep_top_k_, conf_thresh, nms_threshold_, nms_top_k_, visualize_, vis_thresh_):
+    def __init__(self, num_classes, background_label, keep_top_k, conf_thresh, nms_threshold, nms_top_k):
         #super(Detect, self).__init__()
-        # initializations ........
-        self.num_classes = num_classes_
-        self.share_location = share_location_ or True
-        if share_location_:
-            self.num_loc_classes = 1
-        else:
-            self.num_loc_classes = num_classes_
-        self.background_label_id = background_label_id_
-        self.code_type = code_type_
-        self.variance_encoded_in_target = var_encoded_in_target_
-        self.keep_top_k = keep_top_k_
-
+        self.num_classes = num_classes
+        self.background_label = background_label
+        self.keep_top_k = keep_top_k
         # Parameters used in nms.
-        self.nms_threshold = nms_threshold_
-        if nms_threshold_ <= 0:
-            error('<Detection_Output> nms_threshold must be non negative.')
+        self.nms_threshold = nms_threshold
+        if nms_threshold <= 0:
+            print('<Detection_Output> nms_threshold must be non negative.')
         self.top_k = -1
-        self.nms_top_k = nms_top_k_ or -1
+        self.nms_top_k = nms_top_k or -1
         if self.nms_top_k > 0:
             self.top_k = self.nms_top_k
 
-        self.visualize = visualize_
-        self.vis_thresh = vis_thresh_ or 0.6
-        # reset the data transformer (randomly re-initialize it)
+    def forward(self, loc_data, conf_data, prior_data):
 
-    def forward(self, loc_data, conf_data, box_data):
+        # self.loc_data = loc_data    # batch X num_priors*4
+        # self.conf_data = conf_data  # batch*num_priors X num_classes
+        # self.prior_data = prior_data  # 1 X 2 X num_priors*4
+        num = loc_data.size(0) # batch size
+        self.output = torch.Tensor(num,self.keep_top_k,7)  # TODO: refactor
 
-        self.loc_data = loc_data # batch_size X num_priors*4
-        self.conf_data = conf_data # batch_size*num_priors X num_classes
-        self.prior_data = box_data # 1 X 2 X num_priors*4
-        num = self.loc_data.size(0)
-        self.output = torch.Tensor(num,self.keep_top_k,7) # could be some empty indices, but this way we output 1 tensor
-
-        num_priors = self.prior_data.size(2) // 4      # height dim of priorbox input / 4
-        if not self.loc_data.size(1) == self.prior_data.size(2):
+        num_priors = prior_data.size(2) // 4  # height dim of priorbox input / 4
+        if not loc_data.size(1) == prior_data.size(2):
             print('Number of priors must match number of location predictions.')
 
-
-        # GET LOCATION PREDICTIONS FROM LOC_DATA
-        # (num X num_priors X 4) -- in caffe
-        # (right now num X (num_priors x 4))
-        #print(num)
-        loc_preds = self.loc_data
-
         # GET CONFIDENCE SCORES FROM CONF_DATA
-
-        if num == 1: # If input is only a single image then we need to add the batch dim that we removed for softmax layer
-            conf_preds = self.conf_data.t().contiguous()
-            conf_preds = conf_preds.unsqueeze(0) # size num x 21 x 7308
-            #print(conf_preds.size())
+        # If input is only a single image then we need to add the batch dim
+        # that we removed for softmax layer
+        if num == 1:
+            conf_preds = conf_data.t().contiguous().unsqueeze(0) # size num x 21 x 7308
         else:
-            conf_predictions = self.conf_data.view(num,num_priors,self.num_classes)
-            conf_preds = conf_predictions.transpose(2,1)
+            conf_preds = conf_data.view(num,num_priors,self.num_classes).transpose(2,1)
 
-        # GET PRIOR BBOXES FROM PRIOR_DATA  -- Still thinking of good way to speed this up
+        # GET PRIOR BBOXES FROM PRIOR_DATA
+        prior_bboxes = prior_data[0][0].view(-1,4)    # Shape [7308 x 4]
+        prior_variances = prior_data[0][1].view(-1,4) # Shape [7308 x 4]
 
-        prior_bboxes = self.prior_data[0][0].view(-1,4)  # size 7308 x 4
-        prior_variances = self.prior_data[0][1].view(-1,4) # size 7308 x 4
-        #print(prior_bboxes.size())
+        #TODO get rid of the batch for loop
         # Decode predictions into bboxes.
         num_kept = 0
         for i in range(num):
-            decode_bboxes =  decode_boxes(prior_bboxes,prior_variances,self.code_type,self.variance_encoded_in_target,loc_preds[i])
+            decode_bboxes =  decode_boxes(prior_bboxes,prior_variances,loc_data[i].view(-1,4))
             # For each class, perform nms
-            conf_scores = conf_preds[i]
-
+            conf_scores = conf_preds[i].clone()
             indices = []
             num_det = 0
-            print(self.num_classes)
-            print(self.nms_threshold)
-            print(self.top_k)
             for c in range(self.num_classes):
-                if not (c == self.background_label_id):
-                    #  Populates overlaps and class_index_table
-                    class_index_table = apply_nms(decode_bboxes, conf_scores[c].clone(), self.nms_threshold, self.top_k)
-                    # Class_index_table now contains the indices (with respect to num_priors) of highest scoring and non-overlapping bboxes for a given class
+                if not c == self.background_label:
+                    # Populates overlaps and class_index_table
+                    class_index_table = apply_nms(decode_bboxes, conf_scores[c], self.nms_threshold, self.top_k)
+                    # Class_index_table now contains the indices (with respect to num_priors)
+                    # of highest scoring and non-overlapping bboxes for a given class
                     indices.append(class_index_table)
-                    # indices = (num_classes X num_det(which could vary over classes, thus using table over tensor))
+                    # TODO optimize
+                    # indices = (num_classes X num_det(which could vary over classes,
+                    # so using table over tensor))
                     num_det += class_index_table.size(0)
-
             length = num_det  # length of tensors to be created based on num boxes after nms
-            score_pairs = torch.Tensor(length)  # will score scores and corresponding bbox table indices
+            score_pairs = torch.Tensor(length)  # scores and corresponding bbox table indices
             indices_list = torch.Tensor(length)
             label_list = torch.Tensor(length)
             ref = 0
@@ -97,28 +73,23 @@ class Detect(Function):
                 label = number + 1 # add 1 to avoid background class
                 label_indices = indices[number]  # top bbox table indices for each class
                 for index in range(label_indices.size(0)):
-
-
                     idx = int(label_indices[index])
-                    indices_list[ref] = idx  #  inserting index of highest conf scores into indices list
+                    # inserting index of highest conf scores into indices list
+                    indices_list[ref] = idx
                     assert(idx <= conf_scores[label].size(0))
-
-                    score_pairs[ref] = conf_scores[label][idx]  #  corresp. score is inserted into score_pairs at same location
-                    label_list[ref] = label  # and label is added to label list at same location
-                    #  score_pairs, indices_list, and label_list could be combined into one tensor potentially
+                    # corresp. score is inserted into score_pairs at same location
+                    score_pairs[ref] = conf_scores[label][idx]
+                    # label is added to label list at same location
+                    label_list[ref] = label
                     ref +=1
-
-
             if num_det > self.keep_top_k: # narrow results further
                 length = self.keep_top_k
-
             final_indices = torch.Tensor(length).zero_()
             final_scores = torch.Tensor(length).zero_()
             final_labels = torch.Tensor(length).zero_()
             sort(score_pairs, indices_list, label_list, length, final_scores, final_indices, final_labels)
             num_kept += num_det
 
-            # print(final_indices.size())
             for j in range(final_indices.size(0)):
                 idx = int(final_indices[j])
                 self.output[i][j][0] = i+1
@@ -128,5 +99,4 @@ class Detect(Function):
                 self.output[i][j][4] = max(min(decode_bboxes[idx][1], 1), 0)
                 self.output[i][j][5] = max(min(decode_bboxes[idx][2], 1), 0)
                 self.output[i][j][6] = max(min(decode_bboxes[idx][3], 1), 0)
-
         return self.output
--- model.py
+++ model.py
@@ -11,13 +11,39 @@ import torchvision.models as models
 from torch.utils.serialization import load_lua
 from f_l2norm import L2norm as norm
 import torch.backends.cudnn as cudnn
+import os
 
 
 class SSD(nn.Module):
-    def __init__(self, features1,num_classes):
+    """Single Shot Multibox Architecture
+    The network is composed of a base VGG network followed by the
+    added multibox conv layers.  Each multibox layer branches into
+        1) conv2d for class conf scores
+        2) conv2d for localization predictions
+        3) associated priorbox layer to produce default bounding
+           boxes specific to the layer's feature map size.
+    See: https://arxiv.org/pdf/1512.02325.pdf for more details.
+
+    Args:
+        features1: (Sequential nn.Module) VGG layers for input
+            size of either 300 or 500
+        phase: (string) Can be "test" or "train"
+        size: (int) the SSD version for the input size. Can be 300 or 500.
+            Defaul: 300
+        num_classes: (int) the number of classes to score. Default: 21.
+    """
+
+    def __init__(self, phase, sz=300, num_classes=21):
         super(SSD, self).__init__()
+        self.phase = phase
+        self.size = sz
+        self.num_classes = num_classes
         param=num_classes*3
-        self.features1 = features1
+        self.features1 = build_base(cfg[str(sz)] ,3)
+
+        # TODO: Build the rest of the sequentials in a for loop.
+        v = [0.1, 0.1, 0.2, 0.2] # variances
+        ar = [1,1,2,1/2,3,1/3] # aspect ratios
         self.features2 = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(512,512,kernel_size=3,padding=1),
@@ -32,8 +58,6 @@ class SSD(nn.Module):
             nn.Conv2d(1024,1024,kernel_size=1),
             nn.ReLU(inplace=True),
         )
-        # end vgg
-
         self.features3 = nn.Sequential(
             nn.Conv2d(1024,256,kernel_size=1),
             nn.ReLU(inplace=True),
@@ -58,69 +82,140 @@ class SSD(nn.Module):
 
 
         self.L2norm = L2norm(512,20)
+
         self.l4_3 = nn.Conv2d(512,12,kernel_size=3,padding=1)
         self.c4_3 = nn.Conv2d(512,param,kernel_size=3,padding=1)
-        self.p4_3 = PriorBox(num_classes, 300, 300, 30, -1, [1,2,1/2], [0.1, 0.1, 0.2, 0.2], False, True)
+        self.p4_3 = PriorBox(num_classes, sz, sz, 30, -1, ar[1:4], v, False, True)
 
         self.lfc7 = nn.Conv2d(1024,24,kernel_size=3,padding=1)
         self.cfc7 = nn.Conv2d(1024,param*2,kernel_size=3,padding=1)
-        self.pfc7 = PriorBox(num_classes, 300, 300, 60, 114, [1,1,2,1/2,3,1/3], [0.1, 0.1, 0.2, 0.2], False, True)
-        #
-        self.l6_2=nn.Conv2d(512,24,kernel_size=3,padding=1)
-        self.c6_2=nn.Conv2d(512,param*2,kernel_size=3,padding=1)
-        self.p6_2=PriorBox(num_classes, 300, 300, 114, 168, [1,1,2,1/2,3,1/3], [0.1, 0.1, 0.2, 0.2], False, True)
-        self.l7_2=nn.Conv2d(256,24,kernel_size=3,padding=1)
-        self.c7_2=nn.Conv2d(256,param*2,kernel_size=3,padding=1)
-        self.p7_2=PriorBox(num_classes, 300, 300, 168, 222, [1,1,2,1/2,3,1/3], [0.1, 0.1, 0.2, 0.2], False, True)
-        self.l8_2=nn.Conv2d(256,24,kernel_size=3,padding=1)
-        self.c8_2=nn.Conv2d(256,param*2,kernel_size=3,padding=1)
-        self.p8_2=PriorBox(num_classes, 300, 300, 222, 276, [1,1,2,1/2,3,1/3], [0.1, 0.1, 0.2, 0.2], False, True)
+        self.pfc7 = PriorBox(num_classes, sz, sz, 60, 114, ar, v, False, True)
+
+        self.l6_2 = nn.Conv2d(512,24,kernel_size=3,padding=1)
+        self.c6_2 = nn.Conv2d(512,param*2,kernel_size=3,padding=1)
+        self.p6_2 = PriorBox(num_classes, sz, sz, 114, 168, ar, v, False, True)
+
+        self.l7_2 = nn.Conv2d(256,24,kernel_size=3,padding=1)
+        self.c7_2 = nn.Conv2d(256,param*2,kernel_size=3,padding=1)
+        self.p7_2 = PriorBox(num_classes, sz, sz, 168, 222, ar, v, False, True)
+
+        self.l8_2 = nn.Conv2d(256,24,kernel_size=3,padding=1)
+        self.c8_2 = nn.Conv2d(256,param*2,kernel_size=3,padding=1)
+        self.p8_2 = PriorBox(num_classes, sz, sz, 222, 276, ar, v, False, True)
+
         self.lp6 = nn.Conv2d(256,24,kernel_size=3,padding=1)
         self.cp6 = nn.Conv2d(256,param*2,kernel_size=3,padding=1)
-        self.pp6=PriorBox(num_classes, 300, 300, 276, 330, [1,1,2,1/2,3,1/3], [0.1, 0.1, 0.2, 0.2], False, True)
-        self.softmax = nn.Softmax()
-        self.detect = Detect(21, True, 0, 'CENTER', False, 200, 0.01, 0.45, 400, False, 1)
+        self.pp6 = PriorBox(num_classes, sz, sz, 276, 330, ar, v, False, True)
 
-    def forward(self, x, phase):
+        self.softmax = nn.Softmax()
+        self.detect = Detect(21, 0, 200, 0.01, 0.45, 400)
+
+    def forward(self, x):
+        """Applies network layers and ops on input x.
+
+        Args:
+            x: input image or batch of images. Shape: [batch,3*batch,300,300].
+
+        Return:
+            Depending on phase:
+            test:
+                Variable(tensor) of output class label predictions,
+                confidence score, and corresponding location predictions for
+                each object detected. Shape: [batch,topk,7]
+
+            train:
+                list of concat outputs from:
+                    1: confidence layers, Shape: [batch*num_priors,num_classes]
+                    2: localization layers, Shape: [batch,num_priors*4]
+                    3: priorbox layers, Shape: [2,num_priors*4]
+        """
         x = self.features1(x)
         y = self.L2norm(x)
-        branch1 = [torch.transpose(torch.transpose(self.l4_3(y),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c4_3(y),1,2),2,3).contiguous()]
+        # branch1 = [torch.transpose(torch.transpose(self.l4_3(y),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c4_3(y),1,2),2,3).contiguous()]
+        branch1 = [self.l4_3(y).permute(0,2,3,1), self.c4_3(y).permute(0,2,3,1)]
         p1 = self.p4_3(x)
-        branch1 = [o.view(o.size(0),-1) for o in branch1]
+        branch1 = [o.view(o.contiguous().size(0),-1) for o in branch1]
         x = self.features2(x)
-        branch2 = [torch.transpose(torch.transpose(self.lfc7(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.cfc7(x),1,2),2,3).contiguous()]
+        # branch2 = [torch.transpose(torch.transpose(self.lfc7(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.cfc7(x),1,2),2,3).contiguous()]
+        branch2 = [self.lfc7(x).permute(0,2,3,1), self.cfc7(x).permute(0,2,3,1)]
         p2 = self.pfc7(x)
-        branch2 = [o.view(o.size(0),-1) for o in branch2]
+        branch2 = [o.view(o.contiguous().size(0),-1) for o in branch2]
         x = self.features3(x)
-        branch3 = [torch.transpose(torch.transpose(self.l6_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c6_2(x),1,2),2,3).contiguous()]
+        # branch3 = [torch.transpose(torch.transpose(self.l6_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c6_2(x),1,2),2,3).contiguous()]
+        branch3 = [self.l6_2(x).permute(0,2,3,1), self.c6_2(x).permute(0,2,3,1)]
         p3 = self.p6_2(x)
-        branch3 = [o.view(o.size(0),-1) for o in branch3]
+        branch3 = [o.view(o.contiguous().size(0),-1) for o in branch3]
         x = self.features4(x)
-        branch4 = [torch.transpose(torch.transpose(self.l7_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c7_2(x),1,2),2,3).contiguous()]
+        # branch4 = [torch.transpose(torch.transpose(self.l7_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c7_2(x),1,2),2,3).contiguous()]
+        branch4 = [self.l7_2(x).permute(0,2,3,1), self.c7_2(x).permute(0,2,3,1)]
         p4 = self.p7_2(x)
-        branch4 = [o.view(o.size(0),-1) for o in branch4]
+        branch4 = [o.view(o.contiguous().size(0),-1) for o in branch4]
         x = self.features5(x)
-        branch5 = [torch.transpose(torch.transpose(self.l8_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c8_2(x),1,2),2,3).contiguous()]
+        # branch5 = [torch.transpose(torch.transpose(self.l8_2(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.c8_2(x),1,2),2,3).contiguous()]
+        branch5 = [self.l8_2(x).permute(0,2,3,1), self.c8_2(x).permute(0,2,3,1)]
         p5 = self.p8_2(x)
-        branch5 = [o.view(o.size(0),-1) for o in branch5]
+        branch5 = [o.view(o.contiguous().size(0),-1) for o in branch5]
         x = self.pool6(x)
-        branch6 = [torch.transpose(torch.transpose(self.lp6(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.cp6(x),1,2),2,3).contiguous()]
+        # branch6 = [torch.transpose(torch.transpose(self.lp6(x),1,2),2,3).contiguous(),torch.transpose(torch.transpose(self.cp6(x),1,2),2,3).contiguous()]
+        branch6 = [self.lp6(x).permute(0,2,3,1), self.cp6(x).permute(0,2,3,1)]
         p6 = self.pp6(x)
-        branch6 = [o.view(o.size(0),-1) for o in branch6]
+        branch6 = [o.view(o.contiguous().size(0),-1) for o in branch6]
         loc_layers = torch.cat((branch1[0],branch2[0],branch3[0],branch4[0],branch5[0],branch6[0]),1)
         conf_layers = torch.cat((branch1[1],branch2[1],branch3[1],branch4[1],branch5[1],branch6[1]),1)
         box_layers = torch.cat((p1,p2,p3,p4,p5,p6), 2)
 
-        if phase == "test":
+        if self.phase == "test":
             conf_layers = conf_layers.view(-1,21)
             conf_layers = self.softmax(conf_layers)
             output = self.detect(loc_layers,conf_layers,box_layers)
         else:
-            conf_layers = conf_layers.view(conf_layers.size(0),-1,21)
+            conf_layers = conf_layers.view(conf_layers.size(0),-1,self.num_classes)
+            output = [loc_layers, conf_layers, box_layers]
         return output
 
-# show ref
-def make_layers(cfg, i,  batch_norm=False):
+
+    # This function is very closely adapted from jcjohnson pytorch-vgg conversion script
+    # https://github.com/jcjohnson/pytorch-vgg/blob/master/t7_to_state_dict.py
+    def load_weights(self, base_file, norm_file = './weights/normWeights.t7'):
+        py_modules = list(self.modules())
+        next_py_idx = 0
+        scale_weight = load_lua(norm_file).float()
+        other, ext = os.path.splitext(base_file)
+        if ext == '.t7':
+            print('Loading lua model weights...')
+            other = load_lua(base_file)
+        else:
+            print('Only .t7 is supported for now.')
+            return
+        #elif: ext == ''
+        for i, t7_module in enumerate(other.modules):
+            if not hasattr(t7_module, 'weight'):
+                continue
+            assert hasattr(t7_module, 'bias')
+            while not hasattr(py_modules[next_py_idx], 'weight'):
+                next_py_idx += 1
+            py_module = py_modules[next_py_idx]
+            next_py_idx += 1
+
+            # The norm layer should be the only layer with 1d weight
+            if(py_module.weight.data.dim() == 1):
+                # print('%r Copying data from\n  %r to\n  %r' % (i-1, "L2norm", py_module))
+                # py_module.weight.data.copy_(scale_weight)
+                py_module = py_modules[next_py_idx]
+                next_py_idx += 1
+            assert(t7_module.weight.size() == py_module.weight.size())
+            print('%r Copying data from\n  %r to\n  %r' % (i, t7_module, py_module))
+
+            py_module.weight.data.copy_(t7_module.weight)
+            assert(t7_module.bias.size() == py_module.bias.size())
+            py_module.bias.data.copy_(t7_module.bias)
+        py_modules[-14].weight.data.copy_(scale_weight)
+        print('%r Copying data from\n  %r to\n  %r' % (i-1, "L2norm", py_modules[-14]))
+
+
+# This function is derived from torchvision VGG make_layers()
+# https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+def build_base(cfg, i, batch_norm=False):
     layers = []
     in_channels = i
     for v in cfg:
@@ -139,8 +234,14 @@ def make_layers(cfg, i,  batch_norm=False):
 
 
 cfg = {
-    'A': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512],
+    "300" : [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512],
 }
 
-def build_ssd():
-    return SSD(make_layers(cfg['A'],3),21)
+
+def build_ssd(phase, size, num_classes):
+    if phase != "test" and phase != "train":
+        print("Error: Phase not recognized")
+        return
+    if size != 300:
+        print("Error: Sorry only SSD300 is supported currently!")
+    return SSD(phase, size, num_classes)
