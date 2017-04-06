8ca4fea12aef122c2620db40571633647f5379a8
Ellis Brown <elbrown47@gmail.com>
2017-04-06 01:06:14 -0500

    mean average precision meter for Pascal VOC 2007

--- utils/metrics/voc/voc07mAPmeter.py
+++ utils/metrics/voc/voc07mAPmeter.py
@@ -4,28 +4,27 @@ Ellis Brown, Max deGroot
 """
 
 import torch
-from . import meter
 import numpy as np
 import math
-from . import mAPmeter
+from . import mAPmeter, VOC07APMeter
+
 
 class VOC07mAPMeter(mAPmeter.mAPMeter):
-    """ Mean average precision metric for PASCAL V0C 07 dataset """
-    def __init__(self, *args, **kwargs):
-        super(VOC07mAPMeter, self).__init__(*args, **kwargs)
+    """Mean average precision meter for PASCAL V0C 2007 dataset """
 
-    # def add(self,rec,prec):
-    #     """
-    #    calculate average precision, override the default one,
-    #    special 11-point metric
-    #    Params:
-    #    ----------
-    #    rec : numpy.array
-    #        cumulated recall
-    #    prec : numpy.array
-    #        cumulated precision
-    #    Returns:
-    #    ----------
-    #    ap as float
-    #    """
-    #
+    def __init__(self, ovp_thresh=0.5, use_difficult=False, class_names=None, pred_idx=0):
+        """
+        Param:
+            ovp_thresh (optional, float): overlap threshold for TP
+                (default: 0.5)
+            use_difficult (optional, boolean) use difficult ground-truths if
+                applicable, otherwise just ignore
+                (default: False)
+            class_names (optional, list of str): if provided, will print out AP
+                for each class
+                (default: None)
+            pred_idx (optional, int): prediction index in network output list
+                (default: 0)
+        """
+        self.apmeter = VOC07APMeter(
+            ovp_thresh, use_difficult, class_names, pred_idx)
