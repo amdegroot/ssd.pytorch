8d528685be4da8c2a0c01b6eb26ef0c6c3782a7d
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-10 16:09:11 -0500

    update meters

--- utils/metrics/apmeter.py
+++ utils/metrics/apmeter.py
@@ -1,30 +1,31 @@
-"""Meter for monitoring average precision
-
-inspired by
-https://github.com/torchnet/torchnet/blob/f1d43f6a31d56072c88fecf4d255fca2dccc7458/meter/apmeter.lua
-
-Ellis Brown, Max deGroot
-"""
-
-import torch
+import math
 from . import meter
 import numpy as np
-import math
+import torch
 
 
 class APMeter(meter.Meter):
-    """Average Precision meter
-    measures the average precision per class
     """
+    The APMeter measures the average precision per class.
 
+    The APMeter is designed to operate on `NxK` Tensors `output` and
+    `target`, and optionally a `Nx1` Tensor weight where (1) the `output`
+    contains model output scores for `N` examples and `K` classes that ought to
+    be higher when the model is more convinced that the example should be
+    positively labeled, and smaller when the model believes the example should
+    be negatively labeled (for instance, the output of a sigmoid function); (2)
+    the `target` contains only values 0 (for negative examples) and 1
+    (for positive examples); and (3) the `weight` ( > 0) represents weight for
+    each sample.
+    """
     def __init__(self):
         self.reset()
 
     def reset(self):
         """Resets the meter with empty member variables"""
-        self.scores = torch.DoubleTensor(torch.DoubleStorage())
+        self.scores = torch.FloatTensor(torch.FloatStorage())
         self.targets = torch.LongTensor(torch.LongStorage())
-        self.weights = torch.DoubleTensor(torch.DoubleStorage())
+        self.weights = torch.FloatTensor(torch.FloatStorage())
 
     def add(self, output, target, weight=None):
         """
@@ -40,27 +41,33 @@ class APMeter(meter.Meter):
             weight (optional, Tensor): Nx1 tensor representing the weight for
                 each example (each weight > 0)
         """
-        # assertions on the input
-        if weight:
+        if not torch.is_tensor(output):
+            output = torch.from_numpy(output)
+        if not torch.is_tensor(target):
+            target = torch.from_numpy(target)
+
+        if weight is not None:
+            if not torch.is_tensor(weight):
+                weight = torch.from_numpy(weight)
             weight = weight.squeeze()
         if output.dim() == 1:
-            output = output.unsqueeze(1)
+            output = output.view(output.size(0), 1)
         else:
-            assert output.dims() == 2, \
+            assert output.dim() == 2, \
                 'wrong output size (should be 1D or 2D with one column \
-                perclass)'
+                per class)'
         if target.dim() == 1:
-            target = target.unsqueeze(1)
+            target = target.view(target.size(0), 1)
         else:
-            assert target.dims() == 2, \
+            assert target.dim() == 2, \
                 'wrong target size (should be 1D or 2D with one column \
-                perclass)'
-        if weight:
-            assert weight.dims() == 1, 'Weight dimension should be 1'
+                per class)'
+        if weight is not None:
+            assert weight.dim() == 1, 'Weight dimension should be 1'
             assert weight.numel() == target.size(0), \
                 'Weight dimension 1 should be the same as that of target'
-            assert torch.min(weight) > 0, 'Weight should be non-negative only'
-        assert torch.equal(weight.float().sqrt(), weight.float()), \
+            assert torch.min(weight) >= 0, 'Weight should be non-negative only'
+        assert torch.equal(target**2, target), \
             'targets should be binary (0 or 1)'
         if self.scores.numel() > 0:
             assert target.size(1) == self.targets.size(1), \
@@ -70,56 +77,59 @@ class APMeter(meter.Meter):
         if self.scores.storage().size() < self.scores.numel() + output.numel():
             new_size = math.ceil(self.scores.storage().size() * 1.5)
             new_weight_size = math.ceil(self.weights.storage().size() * 1.5)
-            self.scores.storage().resize_(new_size + output.numel())
-            self.targets.storage().resize_(new_size + output.numel())
-            if weight:
-                self.weights.storage().resize_(new_weight_size + output.size(0))
+            self.scores.storage().resize_(int(new_size + output.numel()))
+            self.targets.storage().resize_(int(new_size + output.numel()))
+            if weight is not None:
+                self.weights.storage().resize_(int(new_weight_size
+                                               + output.size(0)))
 
-        # store scores and targets:
+        # store scores and targets
         offset = self.scores.size(0) if self.scores.dim() > 0 else 0
         self.scores.resize_(offset + output.size(0), output.size(1))
         self.targets.resize_(offset + target.size(0), target.size(1))
-        self.scores.narrow(1, offset + 1, output.size(0)).copy_(output)
-        self.targets.narrow(1, offset + 1, target.size(0)).copy_(target)
+        self.scores.narrow(0, offset, output.size(0)).copy_(output)
+        self.targets.narrow(0, offset, target.size(0)).copy_(target)
 
-        if weight:
+        if weight is not None:
             self.weights.resize_(offset + weight.size(0))
-            self.weights.narrow(1, offset + 1, weight.size(0)).copy_(weight)
+            self.weights.narrow(0, offset, weight.size(0)).copy_(weight)
 
     def value(self):
         """Returns the model's average precision for each class
 
         Return:
-            ap (DoubleTensor): 1xK tensor, with avg precision for each class k
+            ap (FloatTensor): 1xK tensor, with avg precision for each class k
         """
 
-        if not self.scores.numel() == 0:
+        if self.scores.numel() == 0:
             return 0
-        ap = torch.DoubleTensor(self.scores.size(1)).fill(0)
-        rg = torch.range(1, self.scores.size(0)).double()
+        ap = torch.zeros(self.scores.size(1))
+        rg = torch.range(1, self.scores.size(0)).float()
         if self.weights.numel() > 0:
             weight = self.weights.new(self.weights.size())
             weighted_truth = self.weights.new(self.weights.size())
 
-        # compute average precision for each class:
-        for k in range(1, self.scores.size(1)):
-            # sort scores:
+        # compute average precision for each class
+        for k in range(self.scores.size(1)):
+            # sort scores
             scores = self.scores[:, k]
             targets = self.targets[:, k]
-            _, sortind = torch.sort(scores, 0, true)
+            _, sortind = torch.sort(scores, 0, True)
             truth = targets[sortind]
             if self.weights.numel() > 0:
                 weight = self.weights[sortind]
-                weighted_truth = truth.double() * weight
-                rg = weight.cumsum()
+                weighted_truth = truth.float() * weight
+                rg = weight.cumsum(0)
 
             # compute true positive sums
-            tp = weighted_truth.cumsum() if weighted_truth \
-                else truth.double().cumsum()
+            if self.weights.numel() > 0:
+                tp = weighted_truth.cumsum(0)
+            else:
+                tp = truth.float().cumsum(0)
 
             # compute precision curve
-            precision = tp.div_(rg)
+            precision = tp.div(rg)
 
-            # compute average precision:
+            # compute average precision
             ap[k] = precision[truth.byte()].sum() / max(truth.sum(), 1)
         return ap
