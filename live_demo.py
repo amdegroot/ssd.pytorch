8d1e33cee99220316f4c48335cd0ce124701737f
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-10 15:55:32 -0500

    clean up webcam demo

--- live_demo.py
+++ live_demo.py
@@ -14,41 +14,35 @@ from data import base_transform
 import numpy as np
 from ssd import build_ssd
 import cv2
+from sys import platform as sys_pf
 
-parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
-parser.add_argument('--trained_model', default='weights/', type=str, help='Trained state_dict file path to open')
-parser.add_argument('--confidence_threshold', default=0.6, type=float, help='Detection confidence threshold')
-parser.add_argument('--top_k', default=5, type=int, help='Further restrict the number of predictions to parse')
-parser.add_argument('--cuda', default=False, type=bool, help='Use cuda to demo the model')
-args = parser.parse_args()
-
-args.trained_model = 'weights/ssd_300_VOC0712.pth'
+trained_model = 'weights/ssd_300_VOC0712.pth'
 net = build_ssd('test', 300, 21)    # initialize SSD
-net.load_state_dict(torch.load(args.trained_model))
+net.load_state_dict(torch.load(trained_model))
 net.eval()
-transform = base_transform(net.size,(104,117,123))
-colors = [(255, 0, 0),(0, 255, 0),(0, 0, 255)]
+transform = base_transform(net.size, (104, 117, 123))
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+
 def predict(frame):
-    height,width = frame.shape[:2]
+    height, width = frame.shape[:2]
     img = Image.fromarray(frame)
     x = Variable(transform(img).unsqueeze_(0))
     y = net(x)      # forward pass
     detections = y.data
     # scale each detection back up to the image
-    scale = torch.Tensor([width,height,width,height])
-
+    scale = torch.Tensor([width, height, width, height])
     for i in range(detections.size(1)):
         j = 0
-        while detections[0,i,j,0] >= 0.6:
-            pt = (detections[0,i,j,1:]*scale).cpu().numpy()
-            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), colors[i%3], 2)
-            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), font, 2,(255,255,255),2,cv2.LINE_AA)
-            # labels.append(label_name)
-            j+=1
+        while detections[0, i, j, 0] >= 0.6:
+            pt = (detections[0, i, j, 1:]*scale).cpu().numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]),
+                          int(pt[3])), colors[i % 3], 2)
+            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), font,
+                        2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
     return frame
-
 
 
 video_capture = cv2.VideoCapture(0)
@@ -60,11 +54,11 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     frame = predict(frame)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     # Display the resulting frame
     cv2.imshow('Video', frame)
-    cv2.waitKey(5)
+    # cv2.waitKey(5)
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
