92792e1a6c7c797e3555b47c7b4c69e268c4d211
Ellis Brown <elbrown47@gmail.com>
2017-04-27 12:22:55 -0500

    multithreaded video demo

--- live_demo.py
+++ live_demo.py
@@ -2,15 +2,13 @@ from __future__ import print_function
 import sys
 import os
 import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torchvision.transforms as transforms
 from torch.autograd import Variable
 import cv2
-from sys import platform as sys_pf
-from data import VOC_CLASSES as labelmap
-import torch.utils.data as data
+import time
+from imutils.video import FPS, WebcamVideoStream
+
 from data import BaseTransform
+from data import VOC_CLASSES as labelmap
 from ssd import build_ssd
 
 
@@ -33,29 +31,50 @@ def predict(frame):
     for i in range(detections.size(1)):
         j = 0
         while detections[0, i, j, 0] >= 0.6:
-            pt = (detections[0, i, j, 1:]*scale).cpu().numpy()
+            pt = (detections[0, i, j, 1:] * scale).cpu().numpy()
             cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]),
-                          int(pt[3])), colors[i % 3], 2)
-            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), font,
+                                                            int(pt[3])), colors[i % 3], 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), font,
                         2, (255, 255, 255), 2, cv2.LINE_AA)
             j += 1
     return frame
 
 
-video_capture = cv2.VideoCapture(0)
+# start video stream thread, allow buffer to fill
+print("[INFO] starting threaded video stream...")
+stream = WebcamVideoStream(src=0).start()  # default camera
+time.sleep(1.0)
+
+# start fps timer
+fps = FPS().start()
+
+# loop over frames from the video file stream
 while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    # grab next frame
+    frame = stream.read()
+    key = cv2.waitKey(1) & 0xFF
+
+    # update FPS counter
+    fps.update()
     frame = predict(frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    # keybindings for display
+    if key == ord('p'):  # pause
+        while True:
+            key2 = cv2.waitKey(1) or 0xff
+            cv2.imshow('frame', frame)
+            if key2 == ord('p'):  # resume
+                break
+    cv2.imshow('frame', frame)
+    if key == 27:  # exit
         break
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-    cv2.waitKey(1)
-# When everything is done, release the capture
-video_capture.release()
+
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# cleanup
 cv2.destroyAllWindows()
+stream.stop()
