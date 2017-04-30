47cdccf92110f4b955b3503582db356b51c871a3
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-04-30 17:15:10 -0500

    Move modules, functions, and box_utils to layers/ (#14)
    * Move functions and modules to layers/
    
    * Move utils to layers

--- layers/functions/prior_box.py
+++ layers/functions/prior_box.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
-from data import v2,v1
+from data import v2, v1
 from math import sqrt as sqrt
 from itertools import product as product
 if torch.cuda.is_available():
@@ -21,7 +21,8 @@ class PriorBox(object):
         super(PriorBox, self).__init__()
         # self.type = cfg.name
         self.image_size = cfg['min_dim']
-        self.num_priors = len(cfg['aspect_ratios']) # number of priors for feature map location (either 4 or 6)
+        # number of priors for feature map location (either 4 or 6)
+        self.num_priors = len(cfg['aspect_ratios'])
         self.variance = cfg['variance'] or [0.1]
         self.feature_maps = cfg['feature_maps']
         self.min_sizes = cfg['min_sizes']
@@ -38,7 +39,7 @@ class PriorBox(object):
         mean = []
         # TODO merge these
         if self.version == 'v2':
-            for k,f in enumerate(self.feature_maps):
+            for k, f in enumerate(self.feature_maps):
                 for i, j in product(range(f), repeat=2):
                     f_k = self.image_size / self.steps[k]
                     # unit center x,y
@@ -62,29 +63,33 @@ class PriorBox(object):
 
         else:
             # original version generation of prior (default) boxes
-            for i,k in enumerate(self.feature_maps):
+            for i, k in enumerate(self.feature_maps):
                 step_x = step_y = self.image_size/k
                 for h, w in product(range(k), repeat=2):
-                   c_x = ((w+0.5) * step_x)
-                   c_y = ((h+0.5) * step_y)
-                   c_w = c_h = self.min_sizes[i] / 2
-                   s_k = self.image_size # 300
-                   # aspect_ratio: 1,
-                   # size: min_size
-                   mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k, (c_x+c_w)/s_k, (c_y+c_h)/s_k]
-                   if self.max_sizes[i] > 0:
-                       # aspect_ratio: 1
-                       # size: sqrt(min_size * max_size)/2
-                       c_w = c_h = sqrt(self.min_sizes[i] * self.max_sizes[i])/2
-                       mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k, (c_x+c_w)/s_k, (c_y+c_h)/s_k]
-                   # rest of prior boxes
-                   for ar in self.aspect_ratios[i]:
-                       if not (abs(ar-1) < 1e-6):
-                           c_w = self.min_sizes[i] *sqrt(ar)/2
-                           c_h = self.min_sizes[i] /sqrt(ar)/2
-                           mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k, (c_x+c_w)/s_k, (c_y+c_h)/s_k]
+                    c_x = ((w+0.5) * step_x)
+                    c_y = ((h+0.5) * step_y)
+                    c_w = c_h = self.min_sizes[i] / 2
+                    s_k = self.image_size  # 300
+                    # aspect_ratio: 1,
+                    # size: min_size
+                    mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k,
+                             (c_x+c_w)/s_k, (c_y+c_h)/s_k]
+                    if self.max_sizes[i] > 0:
+                        # aspect_ratio: 1
+                        # size: sqrt(min_size * max_size)/2
+                        c_w = c_h = sqrt(self.min_sizes[i] *
+                                         self.max_sizes[i])/2
+                        mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k,
+                                 (c_x+c_w)/s_k, (c_y+c_h)/s_k]
+                    # rest of prior boxes
+                    for ar in self.aspect_ratios[i]:
+                        if not (abs(ar-1) < 1e-6):
+                            c_w = self.min_sizes[i] * sqrt(ar)/2
+                            c_h = self.min_sizes[i] / sqrt(ar)/2
+                            mean += [(c_x-c_w)/s_k, (c_y-c_h)/s_k,
+                                     (c_x+c_w)/s_k, (c_y+c_h)/s_k]
         # back to torch land
-        output = torch.Tensor(mean).view(-1,4)
+        output = torch.Tensor(mean).view(-1, 4)
         if self.clip:
             output.clamp_(max=1, min=0)
         return output
