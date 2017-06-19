ce1e5d62fb1a8bfcacf2781752f8cae56328363e
alexkoltun <alexkoltun@users.noreply.github.com>
2017-06-19 08:22:17 +0300

    Make caffe compatible (#30)
    * Make caffe compatible
    
    * Fix typo

--- data/voc0712.py
+++ data/voc0712.py
@@ -134,12 +134,8 @@ class VOCDetection(data.Dataset):
         if self.transform is not None:
             target = np.array(target)
             img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
-            # to rgb
-            img = img[:, :, (2, 1, 0)]
-            # img = img.transpose(2, 0, 1)
             target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
         return torch.from_numpy(img).permute(2, 0, 1), target, height, width
-        # return torch.from_numpy(img), target, height, width
 
     def pull_image(self, index):
         '''Returns the original image object at index in PIL form
