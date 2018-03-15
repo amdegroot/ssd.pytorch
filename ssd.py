831837af57e51392c1ec3bc76bb8c19f3e7ef3c6
Keith Landry <keith.landry@geniussports.com>
2018-03-15 14:59:01 -0700

    changed the number of boxes to account for only square

--- ssd.py
+++ ssd.py
@@ -194,7 +194,9 @@ extras = {
     '512': [],
 }
 mbox = {
-    '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location
+    # '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location default for 300
+    '300': [2, 2, 2, 2, 2, 2],  # only square boxes
+
     '512': [],
 }
 
