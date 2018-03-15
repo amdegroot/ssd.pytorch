b58838bd295cd2b550fdf5567434b4ec7f0f3cef
Keith Landry <keith.landry@geniussports.com>
2018-03-15 13:54:37 -0700

    test

--- data/bhjc20180123_bball/bhjc.py
+++ data/bhjc20180123_bball/bhjc.py
@@ -161,6 +161,7 @@ class BhjcBballDataset(data.Dataset):
 
         if self.transform is not None:
             target = np.array(target)
+            print(target)
             img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
             # to rgb
             img = img[:, :, (2, 1, 0)]
