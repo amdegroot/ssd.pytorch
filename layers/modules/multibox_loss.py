07a475801dc083ff43187d1586cca6ccdb409021
David Morton <davidlmorton@gmail.com>
2018-10-13 13:05:50 -0500

    Update to pytorch 0.4.0+ compatible

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -94,7 +94,7 @@ class MultiBoxLoss(nn.Module):
         loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
 
         # Hard Negative Mining
-        loss_c[pos] = 0  # filter out pos boxes for now
+        loss_c[pos.view(-1, 1)] = 0  # filter out pos boxes for now
         loss_c = loss_c.view(num, -1)
         _, loss_idx = loss_c.sort(1, descending=True)
         _, idx_rank = loss_idx.sort(1)
@@ -112,6 +112,6 @@ class MultiBoxLoss(nn.Module):
         # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
 
         N = num_pos.data.sum()
-        loss_l /= N
-        loss_c /= N
+        loss_l /= N.float()
+        loss_c /= N.float()
         return loss_l, loss_c
